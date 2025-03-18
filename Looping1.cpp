4ce70d9a67df2cf738eb55292e47df5a643df332
nauflabhy <n4uf4l.ku@gmail.com>
2025-03-18 21:10:41 +0800

    Looping

---
+++ Looping1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int n;
+    int perulanganWhile = 0;
+    int perulanganDo = 0;
+
+
+    cout << "Perulangan for pencacahan naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n =" << n << "Selamat Datang" << endl;
+    }
+}
