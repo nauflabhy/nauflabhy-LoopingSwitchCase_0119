bfc7d845760c1d47ac322f3b7105a8120c4dba95
nauflabhy <n4uf4l.ku@gmail.com>
2025-03-18 21:14:30 +0800

    Looping4

--- Looping1.cpp
+++ Looping1.cpp
@@ -32,4 +32,16 @@ int main()
         n = rand() % 10;
         perulanganWhile++;
     }
+    cout << "Jumlah perulangan = " << perulanganWhile << endl;
+    cout << "Nilai n terakhir = " << n << endl;
+
+    cout << "Perulangan do while" << endl;
+
+    srand(time(0));
+
+    n = rand() % 10;
+
+    cout << "Nilai awal n = " << n << endl;
+
+    do
 }
