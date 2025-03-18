dc71835d26697a5befe7b33d873838d7ed84fd8a
nauflabhy <n4uf4l.ku@gmail.com>
2025-03-18 21:18:30 +0800

    SwitchCase2

--- SwitchCase1.cpp
+++ SwitchCase1.cpp
@@ -8,3 +8,20 @@ float luasPersegiPanjang(float a, float b){
 float luasLingkaran(float a) {
     return 3.14*a*a;
 }
+
+int main(){
+    int pilihan;
+    float panjang, lebar;
+    float jejari;
+
+    do{
+        cout << "Menu" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+
+        cout << "Pilihan (1/2/3) = ";
+        cin >> pilihan;
+
+        switch (pilihan)
+}
